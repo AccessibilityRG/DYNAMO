23e172d9b57e55b6efdfb118fe97bc66fb6a1bc1
htenkanen <henrikki.tenkanen@helsinki.fi>
2018-11-27 12:04:23 +0200

    Minor edits

--- src/Fig3/plot_accessibility_maps_24H.py
+++ src/Fig3/plot_accessibility_maps_24H.py
@@ -34,6 +34,7 @@ License:
   a Creative Commons Attribution 4.0 International License.
   More information about license: http://creativecommons.org/licenses/by-sa/4.0/
 """
+
 import geopandas as gpd
 import pandas as pd
 import pysal as ps
@@ -215,217 +216,221 @@ def extractDfExterior(gdf):
     exterior = gpd.GeoDataFrame([[u_union.exterior]], columns=['geometry'], crs=gdf.crs)
     return exterior
 
-# Parameters
-# ----------
+def main():
 
-show_legend = True
-show_title_text = True
-                
-# Filepaths
-data_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\Travel_time_Maps.shp"
-roads_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\DYNAMO_TLNshp\Tallinn_main_roads_for_visualization.shp"
-boundaries_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\DYNAMO_TLNshp\TLN_bordersDASY.shp"
-water_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\DYNAMO_TLNshp\TLN_water_clip_OSM.shp"
-groceries_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\Groceries.shp"
-outdir = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Figures\Accessibility_Maps\test"
-
-# Read files
-data = gpd.read_file(data_fp)
-roads = gpd.read_file(roads_fp)
-boundaries = gpd.read_file(boundaries_fp)
-water = gpd.read_file(water_fp)
-shops = gpd.read_file(groceries_fp)
-
-# Re-project all into the same crs as grid
-roads['geometry'] = roads['geometry'].to_crs(crs=data.crs)
-roads.crs = data.crs
-
-boundaries['geometry'] = boundaries['geometry'].to_crs(crs=data.crs)
-boundaries.crs = data.crs
-
-water['geometry'] = water['geometry'].to_crs(crs=data.crs)
-water.crs = data.crs
-
-shops['geometry'] = shops['geometry'].to_crs(crs=data.crs)
-shops.crs = data.crs
-
-# Take only largest waterbodies
-water['area'] = water.area
-water = water.sort_values(by='area', ascending=False)
-water.reset_index(inplace=True)
-water = water.ix[0:2]
-
-# Select only primary and trunk roads
-#road_classes = ['trunk', 'primary'] #, 'primary_link', 'secondary']
-#roads = roads.ix[roads['fclass'].isin(road_classes)]
-                 
-# Create Datetime Indices for opening and closing times
-shops = createDateTimeIndex(shops, time_col='open', target_col='opendt')
-shops = createDateTimeIndex(shops, time_col='close', target_col='closedt')
-
-# Create Time Booleans that shows if store is open or not (0/1 dummy-coding)
-shops = createTimeBooleans(shops, starth=0, endh=24, opendt='opendt', closedt='closedt')
-
-# Time columns showing the difference between static and interactive
-tcols = ["H%s" % num for num in range(0,24)]
-
-# Create Custom classifier (bins are the upper boundary of the class (including the value itself))
-my_bins = [5, 10, 15, 30, 45, 60]
-classifier = ps.User_Defined.make(bins=my_bins)
-
-# Classify following columns
-ccolumns= tcols
-
-classif = data[ccolumns].apply(classifier)
-
-# Rename classified column names (add letter c in front)
-classif.columns = list(map(lambda x: "c" + x, classif.columns))
-
-# Join back to grid
-data = data.join(classif)
-
-# Classified time columns showing the difference between static and interactive
-ccols = ["cH%s" % num for num in range(0,24)]
-
-# Rename columns and take the 'H' letter from the beginning away
-data, new_cols = renameTo24HourSystem(data, tcols, minutes=True)
-
-# Create a custom colormap for the map using same colors as in other plots
-# Useful info: http://basemaptutorial.readthedocs.io/en/latest/cmap.html
-# ------------------------------------------------------------------------
-
-# Plot base-grid (with no-data hashes)
-show_noData = False
-
-# ----------
-# Seaborn
-# ----------
-n=7
-# Custom Seaborn colors (however the center needs to be in custom place!)
-palette = sns.diverging_palette(220, 20, n=n)
-
-# Get hex colors
-hex_colors = parseHexSeaborn(palette)
-
-# Reverse
-hex_colors.reverse()
-
-# Plot all time-levels
-# --------------------
-
-N_colors = len(hex_colors)
-
-# Legend labels 
-legend_labels = list(my_bins)
-legend_labels.insert(0,0)
-legend_labels[-1] = "60 <"
-
-for tattribute in new_cols:
-   
-    # Color balancer
-    color_balancer = list(hex_colors)
+    # Parameters
+    # ----------
+    
+    show_legend = True
+    show_title_text = True
+                    
+    # Filepaths
+    data_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\Travel_time_Maps.shp"
+    roads_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\DYNAMO_TLNshp\Tallinn_main_roads_for_visualization.shp"
+    boundaries_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\DYNAMO_TLNshp\TLN_bordersDASY.shp"
+    water_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\DYNAMO_TLNshp\TLN_water_clip_OSM.shp"
+    groceries_fp = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Data_for_figures\Groceries.shp"
+    outdir = r"C:\HY-Data\HENTENKA\KOODIT\manuscripts\2017_DYNAMO\Figures\Accessibility_Maps\test"
+    
+    # Read files
+    data = gpd.read_file(data_fp)
+    roads = gpd.read_file(roads_fp)
+    boundaries = gpd.read_file(boundaries_fp)
+    water = gpd.read_file(water_fp)
+    shops = gpd.read_file(groceries_fp)
+    
+    # Re-project all into the same crs as grid
+    roads['geometry'] = roads['geometry'].to_crs(crs=data.crs)
+    roads.crs = data.crs
+    
+    boundaries['geometry'] = boundaries['geometry'].to_crs(crs=data.crs)
+    boundaries.crs = data.crs
+    
+    water['geometry'] = water['geometry'].to_crs(crs=data.crs)
+    water.crs = data.crs
+    
+    shops['geometry'] = shops['geometry'].to_crs(crs=data.crs)
+    shops.crs = data.crs
+    
+    # Take only largest waterbodies
+    water['area'] = water.area
+    water = water.sort_values(by='area', ascending=False)
+    water.reset_index(inplace=True)
+    water = water.ix[0:2]
+    
+    # Select only primary and trunk roads
+    #road_classes = ['trunk', 'primary'] #, 'primary_link', 'secondary']
+    #roads = roads.ix[roads['fclass'].isin(road_classes)]
+                     
+    # Create Datetime Indices for opening and closing times
+    shops = createDateTimeIndex(shops, time_col='open', target_col='opendt')
+    shops = createDateTimeIndex(shops, time_col='close', target_col='closedt')
+    
+    # Create Time Booleans that shows if store is open or not (0/1 dummy-coding)
+    shops = createTimeBooleans(shops, starth=0, endh=24, opendt='opendt', closedt='closedt')
+    
+    # Time columns showing the difference between static and interactive
+    tcols = ["H%s" % num for num in range(0,24)]
+    
+    # Create Custom classifier (bins are the upper boundary of the class (including the value itself))
+    my_bins = [5, 10, 15, 30, 45, 60]
+    classifier = ps.User_Defined.make(bins=my_bins)
+    
+    # Classify following columns
+    ccolumns= tcols
+    
+    classif = data[ccolumns].apply(classifier)
+    
+    # Rename classified column names (add letter c in front)
+    classif.columns = list(map(lambda x: "c" + x, classif.columns))
+    
+    # Join back to grid
+    data = data.join(classif)
+    
+    # Classified time columns showing the difference between static and interactive
+    ccols = ["cH%s" % num for num in range(0,24)]
+    
+    # Rename columns and take the 'H' letter from the beginning away
+    data, new_cols = renameTo24HourSystem(data, tcols, minutes=True)
+    
+    # Create a custom colormap for the map using same colors as in other plots
+    # Useful info: http://basemaptutorial.readthedocs.io/en/latest/cmap.html
+    # ------------------------------------------------------------------------
+    
+    # Plot base-grid (with no-data hashes)
+    show_noData = False
+    
+    # ----------
+    # Seaborn
+    # ----------
+    n=7
+    # Custom Seaborn colors (however the center needs to be in custom place!)
+    palette = sns.diverging_palette(220, 20, n=n)
+    
+    # Get hex colors
+    hex_colors = parseHexSeaborn(palette)
+    
+    # Reverse
+    hex_colors.reverse()
+    
+    # Plot all time-levels
+    # --------------------
+    
+    N_colors = len(hex_colors)
+    
+    # Legend labels 
+    legend_labels = list(my_bins)
+    legend_labels.insert(0,0)
+    legend_labels[-1] = "60 <"
+    
+    for tattribute in new_cols:
+       
+        # Color balancer
+        color_balancer = list(hex_colors)
+            
+        # Print the classes
+        classcol = "cH%s" % int(tattribute[0:2])
         
-    # Print the classes
-    classcol = "cH%s" % int(tattribute[0:2])
+        # Control the number of colors    
+        classes = list(data[classcol].unique())
+        classes.sort()
+        print("%s \t N-classes: %s \t Classes: " % (tattribute, len(classes)), classes)
     
-    # Control the number of colors    
-    classes = list(data[classcol].unique())
-    classes.sort()
-    print("%s \t N-classes: %s \t Classes: " % (tattribute, len(classes)), classes)
-
-    # If there is no values for all classes, remove the color of the specific class that is missing
-    if len(classes) < N_colors:
-        class_values = [val for val in range(N_colors)]
-        # Put values in reverse order
-        class_values.reverse()
-        # Find out which classes are missing
-        for i in class_values:
-            if not i in classes:
-                del color_balancer[i]
-    # Convert to rgb
-    rgbcolors = [col.hex2color(hexcol) for hexcol in color_balancer]
-
-    # Dynamo colormap
-    Ncolor = len(color_balancer)
-    dynamocmap = LinearSegmentedColormap.from_list("my_colormap", rgbcolors, N=Ncolor, gamma=1.0)
-
-    # Initialize Figure
-    if not show_legend:
-        fig, ax = plt.subplots()
-    else:
-        fig = plt.figure(figsize=(8, 7))
-        # Add axes (1 for image, 2 for custom legend)
-        ax = fig.add_axes([0.05, 0.15, 0.8, 0.65])  #([DistFromLeft, DistFromBottom, Width, Height])
-        ax1 = fig.add_axes([0.2, 0.08, 0.6, 0.035])
+        # If there is no values for all classes, remove the color of the specific class that is missing
+        if len(classes) < N_colors:
+            class_values = [val for val in range(N_colors)]
+            # Put values in reverse order
+            class_values.reverse()
+            # Find out which classes are missing
+            for i in class_values:
+                if not i in classes:
+                    del color_balancer[i]
+        # Convert to rgb
+        rgbcolors = [col.hex2color(hexcol) for hexcol in color_balancer]
     
-    # Column name for shop information
-    name = "h%s" % int(tattribute[0:2])
+        # Dynamo colormap
+        Ncolor = len(color_balancer)
+        dynamocmap = LinearSegmentedColormap.from_list("my_colormap", rgbcolors, N=Ncolor, gamma=1.0)
     
-    # Select only shops that are open at specified time
-    selected_stores = shops.ix[shops[name]==1]
-    
-    if show_noData:
-        # Plot base grid
-        data.plot(ax=ax, color='white', linewidth = 0.1, hatch='x', edgecolor='grey')
-    else:
-        data.plot(ax=ax, color='white', linewidth = 0, edgecolor='grey')
-    
-    # Clip grid with boundaries
-    data = gpd.overlay(data, boundaries, how='intersection')
-    
-    # Plot the map using custom color map (use the classified column)   
-    ax = plotCustomColors(ax=ax, df=data, column=classcol, custom_cmap=dynamocmap, linewidth=0.05, edgecolor='grey')
-    
-    # Plot water bodies
-    water.plot(ax=ax, color='white', alpha=1.0, linewidth=0, edgecolor='grey') #linewidth=0.05
-    
-    # Plot roads
-    roads.plot(ax=ax, color='grey', lw=0.8, alpha=0.8)
-    
-    # Plot shops
-    c = 'black'
-    selected_stores.plot(ax=ax, color=c, marker='o', markersize=12, edgecolor=c)
-    
-    # Specify y and x-lim
-    ax.set_xlim(left=531000, right=553000)
-    ax.set_ylim(top=6596000, bottom=6579400)
-    
-    # Remove tick markers
-    ax.xaxis.set_visible(False)
-    ax.yaxis.set_visible(False)
-    
-    # Info texts
-    info_text = "%s  Stores: %s" % (tattribute, len(selected_stores))
-    ppos_x = 537000; ppos_y = 6596500
-    ax.text(ppos_x, ppos_y, info_text, size=30, color='black', **{'fontname':'Arial'})
-    
-    # Add title text
-    if show_title_text:
-        ax.text(ppos_x+0, ppos_y+2000, "Travel time to closest\ngrocery store in Tallinn", size=20, color='gray', **{'fontname':'Arial'})
-    
-    # Add legend
-    if show_legend:
+        # Initialize Figure
+        if not show_legend:
+            fig, ax = plt.subplots()
+        else:
+            fig = plt.figure(figsize=(8, 7))
+            # Add axes (1 for image, 2 for custom legend)
+            ax = fig.add_axes([0.05, 0.15, 0.8, 0.65])  #([DistFromLeft, DistFromBottom, Width, Height])
+            ax1 = fig.add_axes([0.2, 0.08, 0.6, 0.035])
         
-        ax1.imshow(np.arange(n).reshape(1, n), cmap=mpl.colors.ListedColormap(list(hex_colors)),
-              interpolation="nearest", aspect="auto")
+        # Column name for shop information
+        name = "h%s" % int(tattribute[0:2])
         
-        # Set locations of the bins
-        ax1.set_xticks(np.arange(n+1) - .5)
-        ax1.set_yticks([])
+        # Select only shops that are open at specified time
+        selected_stores = shops.ix[shops[name]==1]
         
-        # Specify the labels
-        ax1.set_xticklabels(legend_labels)
+        if show_noData:
+            # Plot base grid
+            data.plot(ax=ax, color='white', linewidth = 0.1, hatch='x', edgecolor='grey')
+        else:
+            data.plot(ax=ax, color='white', linewidth = 0, edgecolor='grey')
         
-        # Set colorbar title
-        cbar_title = 'Travel time (minutes)'
-        pos_x = 0.25; pos_y = 0.123
-        plt.figtext(pos_x, pos_y, cbar_title, size=12)
-    
-    # Save figure
-    resolution = 500
-    outpath = os.path.join(outdir, "%s_Accessibility_map_%sdpi.png" % (tattribute[0:2], resolution))
-    ax.axis('off')
+        # Clip grid with boundaries
+        data = gpd.overlay(data, boundaries, how='intersection')
         
-    plt.savefig(outpath, dpi=resolution)
-    plt.close()
-    
+        # Plot the map using custom color map (use the classified column)   
+        ax = plotCustomColors(ax=ax, df=data, column=classcol, custom_cmap=dynamocmap, linewidth=0.05, edgecolor='grey')
+        
+        # Plot water bodies
+        water.plot(ax=ax, color='white', alpha=1.0, linewidth=0, edgecolor='grey') #linewidth=0.05
+        
+        # Plot roads
+        roads.plot(ax=ax, color='grey', lw=0.8, alpha=0.8)
+        
+        # Plot shops
+        c = 'black'
+        selected_stores.plot(ax=ax, color=c, marker='o', markersize=12, edgecolor=c)
+        
+        # Specify y and x-lim
+        ax.set_xlim(left=531000, right=553000)
+        ax.set_ylim(top=6596000, bottom=6579400)
+        
+        # Remove tick markers
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        
+        # Info texts
+        info_text = "%s  Stores: %s" % (tattribute, len(selected_stores))
+        ppos_x = 537000; ppos_y = 6596500
+        ax.text(ppos_x, ppos_y, info_text, size=30, color='black', **{'fontname':'Arial'})
+        
+        # Add title text
+        if show_title_text:
+            ax.text(ppos_x+0, ppos_y+2000, "Travel time to closest\ngrocery store in Tallinn", size=20, color='gray', **{'fontname':'Arial'})
+        
+        # Add legend
+        if show_legend:
+            
+            ax1.imshow(np.arange(n).reshape(1, n), cmap=mpl.colors.ListedColormap(list(hex_colors)),
+                  interpolation="nearest", aspect="auto")
+            
+            # Set locations of the bins
+            ax1.set_xticks(np.arange(n+1) - .5)
+            ax1.set_yticks([])
+            
+            # Specify the labels
+            ax1.set_xticklabels(legend_labels)
+            
+            # Set colorbar title
+            cbar_title = 'Travel time (minutes)'
+            pos_x = 0.25; pos_y = 0.123
+            plt.figtext(pos_x, pos_y, cbar_title, size=12)
+        
+        # Save figure
+        resolution = 500
+        outpath = os.path.join(outdir, "%s_Accessibility_map_%sdpi.png" % (tattribute[0:2], resolution))
+        ax.axis('off')
+            
+        plt.savefig(outpath, dpi=resolution)
+        plt.close()
+        
+if __name__ == "__main__":
+    main()            
